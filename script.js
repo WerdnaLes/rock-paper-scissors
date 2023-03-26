c6ed587412f09eb5b34335f1aaeeb351bb7992fa
werdnales <werdnales@gmail.com>
2023-03-27 00:15:04 +0300

    Add game logic, check for correct input

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const button = document.querySelector(".button");
 const winner = document.querySelector(".winner");
+// A regEx to accept input only for rock paper and scissors:
+const inputReg = /^([rR]ock|[pP]aper|[sS]cissors)$/;
 
-button.addEventListener("click", create);
+button.addEventListener("click", () => {
+  const userChoice = capitalize(prompt("Choose your weapon:", ""));
+  //Check if input matches the regEx:
+  if (!inputReg.test(userChoice)) {
+    alert("You can only choose from 'Rock', 'Paper', 'Scissors'");
+    winner.textContent = "";
+    return;
+  }
+  game(userChoice, getComputerChoice());
+});
 
-function create() {
-  winner.textContent = getComputerChoice();
+function winCondition(winText) {
+  winner.textContent = winText;
 
   // Inserts a new element after a particular element:
   //   newEl.style.display="block"
@@ -22,4 +33,37 @@ function getComputerChoice() {
   } else {
     return "Paper";
   }
+}
+
+// Play a game round:
+function game(playerSelection, computerSelection) {
+  if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      winCondition("Player won! Paper beats Rock!");
+    }
+    if (computerSelection === "Scissors") {
+      winCondition("Computer won! Scissors beat Paper!");
+    }
+  }
+  if (playerSelection === "Rock") {
+    if (computerSelection === "Scissors") {
+      winCondition("Player won! Rock beats Scissors!");
+    }
+    if (computerSelection === "Paper") {
+      winCondition("Computer won! Paper beats Rock!");
+    }
+  }
+  if (playerSelection === "Scissors") {
+    if (computerSelection === "Paper") {
+      winCondition("Player won! Scissors beat Paper!");
+    }
+    if (computerSelection === "Rock") {
+      winCondition("Computer won! Rock beats Scissors!");
+    }
+  }
+}
+
+// Capitalize the first letter of your input:
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.substring(1);
 }
